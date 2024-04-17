6ecc2a58db0d403aa93378b0040b0c38741d94bf
tong23316 <tong23316@qq.com>
2024-04-17 14:48:42 +0800

    T043
    优化了首页数据部分js和部分盒子的内部逻辑结构

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,17 @@
 // index.js
-Page({})
+Page({
+  data: {
+    items: [
+      { color: '#f6b059', text: '代办', imgurl:'/imgs/school.svg' },
+      { color: '#8f6aff', text: '待阅', imgurl:'/imgs/score.svg' },
+      { color: '#ec1e71', text: '已办', imgurl:'/imgs/order.svg' },
+      { color: '#03e391', text: '已阅', imgurl:'/imgs/danger.svg' },
+      { color: '#1e9dff', text: '系统', imgurl:'/imgs/all.svg' }
+    ],
+  },
+
+
+
+
+
+})
