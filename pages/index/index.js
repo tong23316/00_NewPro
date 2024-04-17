390a8790f52fd419e92150594a947fb5dd08b69d
tong23316 <tong23316@qq.com>
2024-04-17 15:18:48 +0800

    T044
    优化了横向滑动块的代码结构

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,36 @@ Page({
       { color: '#03e391', text: '已阅', imgurl:'/imgs/danger.svg' },
       { color: '#1e9dff', text: '系统', imgurl:'/imgs/all.svg' }
     ],
+    items2: [
+      {
+        color1: 'rgb(40,233,151)',
+        color2: 'rgb(16,180,145)',
+        imgSrc: '/imgs/myimgs/本子.svg',
+        title: '图书借阅',
+        text: '查看图书借阅记录'
+      },
+      {
+        color1: 'rgb(252,187,44)',
+        color2: 'rgb(255,143,17)',
+        imgSrc: '/imgs/myimgs/图书.svg',
+        title: '图书借阅',
+        text: '查看图书借阅记录'
+      },
+      {
+        color1: 'rgb(22,182,244)',
+        color2: 'rgb(43,141,219)',
+        imgSrc: '/imgs/myimgs/钱包_o.svg',
+        title: '图书借阅',
+        text: '查看图书借阅记录'
+      },
+      {
+        color1: 'rgb(215,116,120)',
+        color2: 'rgb(192,56,45)',
+        imgSrc: '/imgs/myimgs/预警.svg',
+        title: '图书借阅',
+        text: '查看图书借阅记录'
+      }
+    ]
   },
 
 
