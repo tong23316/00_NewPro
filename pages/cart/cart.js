4872a996b356c3980c5e46a39b43dcd9c96be70d
tong23316 <tong23316@qq.com>
2024-04-17 14:48:42 +0800

    T042
    第三页js部分的数据注入已优化结束

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -32,6 +32,13 @@ Page({
         imgSrc:'/imgs/messageimgs/shenpi.png'
       }
     ],
+    items: [
+      { color: '#f6b059', text: '代办', imgurl:'/imgs/school.svg' },
+      { color: '#8f6aff', text: '待阅', imgurl:'/imgs/score.svg' },
+      { color: '#ec1e71', text: '已办', imgurl:'/imgs/order.svg' },
+      { color: '#03e391', text: '已阅', imgurl:'/imgs/danger.svg' },
+      { color: '#1e9dff', text: '系统', imgurl:'/imgs/all.svg' }
+    ],
     // curId:2,
     offsetNum:-134, //这是两个按钮的宽度
   },
